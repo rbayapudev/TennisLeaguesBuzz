47024ea9539d5712032bdc9ab519eee8788b89a5
rbayapudev <rbayapudev@gmail.com>
2025-07-07 14:47:16 -0400

    Create player-registrants.js

---
+++ prodrivetennis/2025/js/player-registrants.js
@@ -0,0 +1,37 @@
+// Player data for Singles Participants
+const singlesParticipants = [
+    { id: 1, name: "Abhinav Sathiamoorthy", email: "asathiam@mathworks.com" },
+    { id: 2, name: "Anjana Srivastava", email: "asrivast@mathworks.com" },
+    { id: 3, name: "Bindusha Bommareddy", email: "bindusha.b@gmail.com" },
+    { id: 4, name: "Brian Demar Jones", email: "bdemarjones82@gmail.com" },
+    { id: 5, name: "Cameron Stabile", email: "cameron.stabile@gmail.com" },
+    { id: 6, name: "Gitesh Nandre", email: "gnandre@mathworks.com" },
+    { id: 7, name: "Joshua Nasman", email: "joshua.nasman@gmail.com" },
+    { id: 8, name: "Kevin McCauley", email: "kevinmccauley2007@gmail.com" },
+    { id: 9, name: "Leah Kazenmayer", email: "mayer200121@gmail.com" },
+    { id: 10, name: "Noah Sung", email: "noahsung99@gmail.com" },
+    { id: 11, name: "Paola Donis Noriega", email: "paodonis@hotmail.com" },
+    { id: 12, name: "Riccardo Consolo", email: "rconsolo@mathworks.com" },
+    { id: 13, name: "Robbie Gold", email: "18goldr@gmail.com" },
+    { id: 14, name: "Saurabh Gujare", email: "gujare.s@northeastern.edu" },
+    { id: 15, name: "Sean Murphy", email: "sean.murphy@berkeley.edu" },
+    { id: 16, name: "Shubham Mankar", email: "smankar@mathworks.com" },
+    { id: 17, name: "Tanner Bradford", email: "wtannerb@verizon.net" },
+    { id: 18, name: "Trenton Squires", email: "trenton.c.squires@gmail.com" },
+    { id: 19, name: "Yeshwanth Devara", email: "yesh.devara@gmail.com" },
+    { id: 20, name: "Zeeshan Khan", email: "zskone77@gmail.com" }
+];
+
+// Player data for Doubles Participants
+const doublesParticipants = [
+    { id: 1, player1: "Anjana Srivastava", email1: "asrivast@mathworks.com", player2: "Abhinav Sathiamoorthy", email2: "asathiam@mathworks.com" },
+    { id: 2, player1: "Bindusha Bommareddy", email1: "bindusha.b@gmail.com", player2: "Yeshwanth Devara", email2: "yesh.devara@gmail.com" },
+    { id: 3, player1: "Brian Demar Jones", email1: "bdemarjones82@gmail.com", player2: "Gitesh Nandre", email2: "gnandre@mathworks.com" },
+    { id: 4, player1: "Cameron Stabile", email1: "cameron.stabile@gmail.com", player2: "David Fern", email2: "davidfink314@gmail.com" },
+    { id: 5, player1: "Dewi Nilamsari", email1: "dnilamsari23@gmail.com", player2: "Noah Sung", email2: "noahsung99@gmail.com" },
+    { id: 6, player1: "Ileen Thelen", email1: "ipinhasi@gmail.com", player2: "Mike", email2: "" },
+    { id: 7, player1: "Kevin McCauley", email1: "kevinmccauley2007@gmail.com", player2: "Zeeshan Khan", email2: "zskone77@gmail.com" },
+    { id: 8, player1: "Riccardo Consolo", email1: "rconsolo@mathworks.com", player2: "Paola Donis Noriega", email2: "paodonis@hotmail.com" },
+    { id: 9, player1: "Saurabh Gujare", email1: "gujare.s@northeastern.edu", player2: "Trenton Squires", email2: "trenton.c.squires@gmail.com" },
+    { id: 10, player1: "Tanner Bradford", email1: "wtannerb@verizon.net", player2: "Tori York", email2: "tori.york20@gmail.com" }
+];
